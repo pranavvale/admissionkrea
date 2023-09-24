337e3860c8fc8587803747f7618afc217795144e
pranavvale <valepranav@gmail.com>
2023-09-24 23:16:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,6 @@ int main() {
     std::cout << "Enter student ID: ";
     std::cin >> studentId;
 
-    // Dynamic memory allocation for the Student object
     Student* student = new Student(studentName, studentId);
 
     double quizScore;
@@ -20,15 +19,13 @@ int main() {
     std::cout << "Enter quiz score: ";
     std::cin >> quizScore;
 
-    // Set the quiz score using a member function
+
     student->setQuizScore(quizScore);
 
     std::cout << "\nStudent Grade Report:\n";
 
-    // Display student information
     student->displayInfo();
 
-    // Don't forget to free the allocated memory
     delete student;
 
     return 0;
