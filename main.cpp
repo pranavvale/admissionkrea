b31d09ed1e57ef044a586204ad8b049c74faa79e
pranavvale <valepranav@gmail.com>
2023-09-24 23:16:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "Student.h"
 #include <iostream>
 #include <string>
+
 int main() {
     std::string studentName;
     int studentId;
@@ -11,16 +12,24 @@ int main() {
     std::cout << "Enter student ID: ";
     std::cin >> studentId;
 
-    Student student(studentName, studentId);
+    // Dynamic memory allocation for the Student object
+    Student* student = new Student(studentName, studentId);
 
     double quizScore;
 
     std::cout << "Enter quiz score: ";
     std::cin >> quizScore;
-    student.setQuizScore(quizScore);
+
+    // Set the quiz score using a member function
+    student->setQuizScore(quizScore);
 
     std::cout << "\nStudent Grade Report:\n";
-    student.displayInfo();
+
+    // Display student information
+    student->displayInfo();
+
+    // Don't forget to free the allocated memory
+    delete student;
 
     return 0;
 }
